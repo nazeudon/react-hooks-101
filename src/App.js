998643038c385727dabcf79e18a251b21c430037
nazeudon <nazeudon48112@icloud.com>
2020-10-07 22:24:58 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment2 = () => setCount((prevCount) => prevCount + 1);
-  const decrement2 = () => setCount((prevCount) => prevCount - 1);
-
-  const reset = () => setCount(0);
-
-  const double = () => setCount((prevCount) => prevCount * 2);
-
-  const divide3 = () =>
-    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;
