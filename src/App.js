be1db68d485a087eed8bb4f12bc2b6e2c31a5c5f
nazeudon <nazeudon48112@icloud.com>
2020-10-07 10:09:31 +0900

    my first react hook with using state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a template for react app.</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((prevCount) => prevCount + 1);
+  const decrement2 = () => setCount((prevCount) => prevCount - 1);
+
+  const reset = () => setCount(0);
+
+  const double = () => setCount((prevCount) => prevCount * 2);
+
+  const divide3 = () =>
+    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
