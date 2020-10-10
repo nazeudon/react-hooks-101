d86372f5ff04eda0f2239830b8e742f92e4c095a
nazeudon <nazeudon48112@icloud.com>
2020-10-10 10:27:47 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,20 @@ const App = () => {
     setBody("");
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      "全てのイベントを本当に削除しても良いですか？"
+    );
+    if (result) {
+      dispatch({
+        type: "DELETE_ALL_EVENTS",
+      });
+    }
+  };
+
+  const unCreatable = title === "" || body === "";
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -43,10 +57,20 @@ const App = () => {
             onChange={(e) => setBody(e.target.value)}
           />
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          全てのイベントを削除する
+        </button>
       </form>
 
       <h4>イベント一覧</h4>
@@ -60,8 +84,8 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {state.map((event) => (
-            <Event event={event} dispatch={dispatch} />
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
           ))}
         </tbody>
       </table>
